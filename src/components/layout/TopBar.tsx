cd44cba99d6db7b58caea28e73c110efe8724c7b
nevermature <nevermature01@gmail.com>
2026-02-09 09:59:08 +0000

    hide the emails in mobile version (_for element <TopBar>_)

--- src/components/layout/TopBar.tsx
+++ src/components/layout/TopBar.tsx
@@ -5,11 +5,11 @@ import { Mail, Phone } from "lucide-react";
 export const TopBar = () => (
     <div className="bg-[#1F3C3C] text-gray-300 text-xs font-medium">
       <div className="container max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-wrap justify-center items-center h-auto sm:h-10 py-2 sm:py-0 gap-x-8 gap-y-2">
-        <a href="mailto:Nursing@jkhospital.in" className="flex items-center gap-2 hover:text-white">
+        <a href="mailto:Nursing@jkhospital.in" className="hidden sm:flex items-center gap-2 hover:text-white">
           <Mail className="h-4 w-4" />
           <span>Nursing@jkhospital.in</span>
         </a>
-        <a href="mailto:Care@jkhospital.in" className="flex items-center gap-2 hover:text-white">
+        <a href="mailto:Care@jkhospital.in" className="hidden sm:flex items-center gap-2 hover:text-white">
           <Mail className="h-4 w-4" />
           <span>Care@jkhospital.in</span>
         </a>
